38e4b9fd5ab4396dfafe488af74750f6265077ea
Manuel Pagés Gonzales <m.pago92@gmail.com>
2021-04-05 16:29:28 +0200

    added getter and setter to GameObject class

--- scripts/valueObjects/GameObject.js
+++ scripts/valueObjects/GameObject.js
@@ -7,44 +7,43 @@ class GameOBject{
         this.height = 0;        
     }
 // Getter & Setter
-
+    //image
  
     getImg(){
         return this.img;
     }
-
     setImg(img){
         this.img = img;
     }
 
+    // x position
     getPosX(){
         return this.posX;
     }
-
     setPosX(posX){
         this.posX = posX;
     }
 
+    // y position
     getPosY(){
         return this.posX;
     }
-
     setPosY(posY){
         this.posY = posY;
     }
 
+    // width of object
     getWidth(){
         return this.width;
     }
-
     setWidth(width){
         this.width = width;
     }
 
+    // height of object
     getHeight(){
         return this.height;
     }
-
     setHeight(height){
         this.height = height;
     }
