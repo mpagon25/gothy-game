6c20d64bfa4cb6cef1558a21a5c73900e33af987
Manuel Pagés Gonzales <m.pago92@gmail.com>
2021-06-22 17:13:45 +0200

    create CollisionService class and move related methods to the class

--- scripts/PlayerService.js
+++ scripts/PlayerService.js
@@ -1,12 +1,12 @@
 class PlayerService{
     constructor(){
         this.drawer = new Draw();
-    }
+    };
 
     addPlayer(){
         let newPlayer = new Player(0,0, ImageLoader.getImage('pLeft'), ImageLoader.getImage('pRight'));
         return newPlayer;
-    }
+    };
 
 
     turnLeft(player, game){
@@ -14,72 +14,23 @@ class PlayerService{
         player.img = player.leftImg;
         this.drawer.drawScene(game);
         return player;
-    }
+    };
 
     turnRight(player, game){
         player.looksRight = true;
         player.img = player.rightImg;
         this.drawer.drawScene(game);
         return player;
-    }
+    };
 
     moveDown(player, value){
         player.posY = player.posY + value;
         this.drawer.drawScene(game);
         return player;
 
-    }
-
-    playerBorderCollision(player, ctx, game){
-        if(player.posY + player.height > ctx.canvas.height ){
-            player.posY = ctx.canvas.height - player.height;
-            game.isGameOver = true;  
-            return;
-
-        }
-        if(player.posY < 0 ){
-            player.posY = 0;            
-
-        }        
-        if(( player.posX + player.width > ctx.canvas.width - 90 ) ){             
-            player.posX = ctx.canvas.width - player.width - 95;            
-
-        }
-        if(( player.posX < 0) ){          
-            player.posX = 0;
-            
-
-        }
-        
-
-    }
-
-    playerBugCollision(player, bugs, game){
-
-        bugs.forEach(( bug )=>{
-          
-            if(player.posX + player.width > bug.posX &&  player.posX < bug.posX + bug.width){                
-                if(player.posY + player.height > bug.posY && player.posY < bug.posY + bug.width){
-                    game.isGameOver = true;
-                    return; 
-                    
-
-                    
-                }
-    
-            }
-
-        });
-
-
-
-
-    }
-
+    };
 
     flyAnimation(){
-        
-
 
         if(this.count > 0){
             this.intervalId = requestAnimationFrame(this.flyAnimation.bind(this));
@@ -106,14 +57,14 @@ class PlayerService{
             cancelAnimationFrame(this.intervalId);
         }
 
-    }
+    };
 
     playerFly(game){
         this.game = game;
         this.count = 10;
         this.flyAnimation();
         this.intervalId = 0;
-    }
+    };
 
 
 
