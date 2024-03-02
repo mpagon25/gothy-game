d7d0754bfb926af18dfae483aa9fb3bb6be5e3a2
Manuel Pages Gonzales <m.pago92@gmail.com>
2024-03-02 03:23:52 +0100

    Improve moving of player. Add Enemy and EnemyService class. Implement logic for adding enemies to the game

--- scripts/valueObjects/Player.js
+++ scripts/valueObjects/Player.js
@@ -2,52 +2,11 @@ class Player {
   constructor(startX, startY, leftImage, rightImage) {
     this._posX = startX;
     this._posY = startY;
-    this.currentImg = rightImage;
+    this._currentImg = rightImage;
     this._isLookingFwrd = true;
-    this.leftImg = leftImage;
-    this.rightImg = rightImage;
-    this._width = this.rightImg.width;
-    this._height = this.rightImg.height;
-  }
-  //
-
-  set posX(x) {
-    this._posX = x;
-  }
-
-  get posX() {
-    return this._posX;
-  }
-
-  set posY(y) {
-    this._posY = y;
-  }
-
-  get posY() {
-    return this._posY;
-  }
-
-  set img(img) {
-    this.currentImg = img;
-  }
-
-  get img() {
-    return this.currentImg;
-  }
-
-  set isLookingFwrd(bool) {
-    this._isLookingFwrd = bool;
-  }
-
-  get isLookingFwrd() {
-    return this._isLookingFwrd;
-  }
-
-  get width() {
-    return this._width;
-  }
-
-  get height() {
-    return this._height;
+    this._leftImg = leftImage;
+    this._rightImg = rightImage;
+    this._width = this._rightImg.width;
+    this._height = this._rightImg.height;
   }
 }
